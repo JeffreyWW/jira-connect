102cf0ded8004c018bb0106c74495bd15d32a3e3
GitHub Action <action@github.com>
2023-01-08 08:17:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/jeffreyww/jiraconnect/MyBundle.kt
+++ src/main/kotlin/com/github/jeffreyww/jiraconnect/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jeffreyww.jiraconnect
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jeffreyww/jiraconnect/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jeffreyww/jiraconnect/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jeffreyww.jiraconnect.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jeffreyww.jiraconnect.MyBundle
 
 class MyApplicationService {
 
